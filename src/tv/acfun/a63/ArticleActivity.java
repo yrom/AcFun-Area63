000fb67f7a240d1c4f93c6ff65434e79676e3d37
Yrom Wang <yrom@qq.com>
2013-09-03 14:10:38 +0800

    add JS interface to view comment and large image;customize web image cache

--- src/tv/acfun/a63/ArticleActivity.java
+++ src/tv/acfun/a63/ArticleActivity.java
@@ -16,27 +16,36 @@
 
 package tv.acfun.a63;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import tv.acfun.a63.api.ArticleApi;
 import tv.acfun.a63.api.entity.Article;
 import tv.acfun.a63.api.entity.Article.SubContent;
 import tv.acfun.a63.util.ActionBarUtil;
 import tv.acfun.a63.util.Connectivity;
+import tv.acfun.a63.util.FileUtil;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewGroup.LayoutParams;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
@@ -73,6 +82,7 @@ import com.android.volley.toolbox.HttpHeaderParser;
  * 
  */
 public class ArticleActivity extends SherlockActivity implements Listener<Article>, ErrorListener {
+    private static String ARTICLE_PATH ;
     public static void start(Context context, int aid) {
         Intent intent = new Intent(context, ArticleActivity.class);
         intent.putExtra("aid", aid);
@@ -81,11 +91,12 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
 
     private Request<?> request;
     private Document mDoc;
-
+    private List<String> imgUrls;  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        ARTICLE_PATH = AcApp.getExternalCacheDir("article").getAbsolutePath();
         ActionBarUtil.setXiaomiFilterDisplayOptions(getSupportActionBar(), false);
         int aid = getIntent().getIntExtra("aid", 0);
         if (aid == 0) {
@@ -99,12 +110,11 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
             setContentView(R.layout.activity_article);
             mWeb = (WebView) findViewById(R.id.webview);
             mWeb.getSettings().setAllowFileAccess(true);
-            mWeb.getSettings().setAppCachePath(
-                    AcApp.getExternalCacheDir("article").getAbsolutePath());
+            mWeb.getSettings().setAppCachePath(ARTICLE_PATH);
             // TODO 无图模式
-            // web.getSettings().setBlockNetworkImage(true);
+            mWeb.getSettings().setBlockNetworkImage(true);
             mWeb.getSettings().setJavaScriptEnabled(true);
-
+            mWeb.addJavascriptInterface(new ACJSObject(), "AC");
             mWeb.setWebChromeClient(new WebChromeClient() {
 
                 @Override
@@ -127,6 +137,17 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
                     // TODO Auto-generated method stub
                     return super.shouldOverrideUrlLoading(view, url);
                 }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    // TODO Auto-generated method stub
+                    if(imgUrls == null && imgUrls.isEmpty())
+                        return;
+                    Log.i(TAG, "on finished:"+url);
+                    String[] arr = new String[imgUrls.size()];
+                    new DownloadImageTask().execute(imgUrls.toArray(arr));
+                }
+                
 
             });
             mWeb.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
@@ -242,7 +263,9 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
     @Override
     public void onResponse(Article response) {
         mArticle = response;
+        imgUrls = response.imgUrls;
         new BuildDocTask().execute(mArticle);
+        
 
     }
 
@@ -259,6 +282,10 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
             try {
                 InputStream in = getAssets().open("article.html");
                 mDoc = Jsoup.parse(in, "utf-8", "");
+                if(imgUrls !=null)
+                    imgUrls.clear();
+                else
+                    imgUrls = new ArrayList<String>();
                 Element title = mDoc.getElementById("title");
                 title.append(buildTitle(params[0]));
                 Element content = mDoc.getElementById("content");
@@ -272,8 +299,41 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
                         content.append("<h2 class=\"article-subtitle\">"+sub.subTitle +"</h2><hr>");
                     }
                     content.append(sub.content);
-                    if(content.select("img").hasAttr("usemap")){
+                    Elements imgs = content.select("img");
+                    if(imgs.hasAttr("usemap")){
                         hasUseMap = true;
+                    }else{
+                        for(int imgIndex=0;imgIndex<imgs.size();imgIndex++){
+                            Element img = imgs.get(imgIndex);
+                            String src = img.attr("src");
+                            imgUrls.add(src); // 
+                            img.attr("org", src);
+                            img.attr("src","file:///android_asset/loading.gif");
+                            img.attr("loc",FileUtil.getLocalFileUri(new File(AcApp.getExternalCacheDir(AcApp.IMAGE),FileUtil.getName(src))).toString());
+                            // 给 img 标签加上点击事件
+                            try {
+                                if ("icon".equals(img.attr("class"))
+                                        || Integer.parseInt(img.attr("width")) < 100
+                                        || Integer.parseInt(img.attr("height")) < 100) {
+                                    continue;
+                                }
+                            } catch (Exception e) {
+                            }
+                            if (src.contains("emotion/images/"))
+                                continue;
+                            // 统一宽度
+                            img.attr("width", "90%");
+                            // 过滤掉图片的url跳转
+                            if (img.parent() != null
+                                    && img.parent().tagName().equalsIgnoreCase("a")) {
+                                img.parent().attr("href",
+                                        "javascript:window.AC.viewImage('" + src + "');");
+                            } else {
+                                img.attr("onclick", "javascript:window.AC.viewImage(this.src);");
+                            }
+                        }
+                        
+                        
                     }
                 }
                 
@@ -300,4 +360,108 @@ public class ArticleActivity extends SherlockActivity implements Listener<Articl
         
         
     };
+    /**
+     * 异步下载图片到缓存目录
+     * @author Yrom
+     *
+     */
+    private class DownloadImageTask extends AsyncTask<String, Integer, Void>{
+
+        private int timeoutMs = 3000;
+
+        @Override
+        protected Void doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            for(int index=0;index<params.length;index++){
+                String url = params[index];
+                if(!url.startsWith("http")){
+                    url = "http://www.acfun.tv"+url;
+                }
+                File cache = new File(AcApp.getExternalCacheDir(AcApp.IMAGE),FileUtil.getName(url));
+                if(cache.exists() && cache.canRead()){
+                    publishProgress(index);
+                    continue;
+                }else{
+                    try {
+                        cache.getParentFile().mkdirs();
+                        cache.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    
+                }
+                InputStream in = null;
+                OutputStream out = null;
+                try {
+                    URL parsedUrl = new URL(url);
+                    HttpURLConnection connection = (HttpURLConnection) parsedUrl.openConnection();
+                    connection.setConnectTimeout(timeoutMs);
+                    connection.setReadTimeout(timeoutMs*2);
+                    connection.setUseCaches(false);
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == 200) {
+                        in = connection.getInputStream();
+                        out = new FileOutputStream(cache);
+                        FileUtil.copyStream(in,out);
+                        publishProgress(index);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (in != null)
+                            in.close();
+                    } catch (IOException e) {
+                    }
+                    try {
+                        if (out != null)
+                            out.close();
+                    } catch (IOException e) {
+                    }
+                }
+            
+            }
+            return null;
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            if(imgUrls != null){
+                String url = imgUrls.get(values[0]);
+                if(url== null) return;
+                Log.i(TAG, url+"cached");
+                mWeb.loadUrl("javascript:(function(){"
+                        + "var images = document.getElementsByTagName(\"img\"); "
+                        + "var imgSrc = images[" + values[0] + "].getAttribute(\"org\"); "
+                        + "if(imgSrc == \"" + url + "\"){ " 
+                        +   "images[" + values[0]+ "].setAttribute(\"src\",images[" + values[0]+"].getAttribute(\"loc\"));}"
+                        + "})()"); 
+            }
+        }
+        @Override  
+        protected void onPostExecute(Void result) {  
+            //确保所有图片都顺利的显示出来  
+            mWeb.loadUrl("javascript:(function(){"
+                    + "var images = document.getElementsByTagName(\"img\"); "
+                    + "for(var i=0;i<images.length;i++){"
+                    +   "var imgSrc = images[i].getAttribute(\"loc\"); "
+                    +   "images[i].setAttribute(\"src\",imgSrc);"
+                    + "}"
+                    + "})()");
+        }  
+        
+    }
+    class ACJSObject{
+        @android.webkit.JavascriptInterface
+        public void viewcomment(){
+            AcApp.showToast("查看评论: ac%d",mArticle.id);
+        }
+        @android.webkit.JavascriptInterface
+        public void viewImage(String url){
+            AcApp.showToast("查看图片: url=%s",url);
+        }
+    }
 }
---
+++ src/tv/acfun/a63/util/FileUtil.java
@@ -0,0 +1,163 @@
+package tv.acfun.a63.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+import tv.acfun.a63.AcApp;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StatFs;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class FileUtil {
+    public static Uri getLocalFileUri(File file){
+        return Uri.fromFile(file);
+    }
+    public static long getFolderSize(File folder) {
+        long size = 0;
+        File[] files = folder.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory())
+                size = size + getFolderSize(files[i]);
+            else
+                size = size + files[i].length();
+        }
+        return size;
+    }
+
+    /*** 格式化文件大小(xxx.xx B/KB/MB/GB) */
+    public static String formatFileSize(long size) {
+        if(size <=0) return "0B";
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        if (size < _1KB)
+            fileSizeString = df.format((double) size) + "B";
+        else if (size < _1MB)
+            fileSizeString = df.format((double) size / _1KB) + "KB";
+        else if (size < _1GB)
+            fileSizeString = df.format((double) size / _1MB) + "MB";
+        else
+            fileSizeString = df.format((double) size / _1GB) + "GB";
+
+        return fileSizeString;
+    }
+
+    /**
+     * 显示SD卡剩余空间
+     * 
+     * @return SD卡不存在则返回null
+     */
+    public static String showFileAvailable() {
+        long availableSize = getExternalAvailable();
+        if (availableSize > 0)
+            return formatFileSize(availableSize);
+        return null;
+    }
+
+    /**
+     * 获得SD卡剩余空间
+     * 
+     * @return SD卡未挂载则返回-1
+     */
+    public static long getExternalAvailable() {
+        if (AcApp.isExternalStorageAvailable()) {
+            StatFs sf = new StatFs(Environment.getExternalStorageDirectory().getPath());
+            long blockSize = sf.getBlockSize();
+            long availCount = sf.getAvailableBlocks();
+            return availCount * blockSize;
+        } else
+            return -1;
+
+    }
+    /**
+     *   "/" ~ "?"之间的".xxx"
+     * @param url
+     * @return
+     */
+    public static String getUrlExt(String url){
+        
+        if (!TextUtils.isEmpty(url)) {
+//            int start = url.lastIndexOf('.');
+            int start = url.lastIndexOf('/');
+            int end = url.lastIndexOf('?');
+            end = end <= start ? url.length() : end;
+            String ext = "";
+            if (start > 0 && start < url.length() - 1) {
+                try{
+                ext = url.substring(start, end).toLowerCase();
+                
+                return ext.substring(ext.lastIndexOf('.'));
+                }catch (StringIndexOutOfBoundsException e) {
+                   Log.e("Util", "when get url ext : "+url,e);
+                }
+            }
+            
+        }
+        return "flv";
+    }
+    
+    public static String guessVideoMimetype(String ext){
+        String mimetype = null;
+        if(".flv".equals(ext)){
+            mimetype = "video/x-flv";
+        }else if(".f4v".equals(ext)){
+            mimetype = "video/x-f4v";
+        }else if(".mp4".equals(ext)){
+            mimetype = "video/mp4";
+        }else mimetype = "video/*";
+/*        else if(".hlv".equals(ext)){
+            mimetype = "video/x-f4v"; // XXX: mimetype of hlv???
+        }*/
+        return mimetype;
+    }
+    public static final int _1KB = 1024;
+    public static final int _1MB = _1KB * _1KB;
+    public static final int _1GB = _1KB * _1MB;
+    /**
+     * @param type the http header, content-type
+     * @return
+     */
+    public static String getMimeType(String type) {
+        if (type == null) {
+            return null;
+        }
+
+        type = type.trim().toLowerCase(Locale.US);
+
+        final int semicolonIndex = type.indexOf(';');
+        if (semicolonIndex != -1) {
+            type = type.substring(0, semicolonIndex);
+        }
+        return type;
+    }
+    public static String getName(String url) {
+        if (!TextUtils.isEmpty(url)) {
+          int start = url.lastIndexOf('/');
+          int end = url.lastIndexOf('?');
+          end = end <= start ? url.length() : end;
+          String name = "";
+          if (start > 0 && start < url.length() - 1) {
+              try{
+              name = url.substring(start, end).toLowerCase();
+              return name;
+              }catch (StringIndexOutOfBoundsException e) {
+                 Log.e("Util", "when get url name : "+url,e);
+              }
+          }
+        }
+        return String.valueOf(url.hashCode())+".jpg";
+    }
+    public static void copyStream(InputStream in, OutputStream out) throws IOException {
+        byte[] buf = new byte[4*_1KB];
+        int len = -1;
+        while((len = in.read(buf))!=-1){
+            out.write(buf,0,len);
+        }
+        buf = null;
+    }
+}
